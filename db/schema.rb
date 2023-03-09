fe55d74926a661d279792dde44b6cbd0fdb6f983
German Gallardo <ggallardoh.90@gmail.com>
2023-03-09 12:47:05 -0300

    Added initial schema

---
+++ db/schema.rb
@@ -0,0 +1,56 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_151239) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "deliveries", force: :cascade do |t|
+    t.string "recipient"
+    t.string "address"
+    t.datetime "delivered_at"
+    t.string "aasm_state"
+    t.bigint "trip_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["trip_id"], name: "index_deliveries_on_trip_id"
+  end
+
+  create_table "pickups", force: :cascade do |t|
+    t.string "sender"
+    t.string "address"
+    t.datetime "picked_up_at"
+    t.string "aasm_state"
+    t.bigint "trip_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["trip_id"], name: "index_pickups_on_trip_id"
+  end
+
+  create_table "routes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "trips", force: :cascade do |t|
+    t.integer "order"
+    t.bigint "route_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["route_id"], name: "index_trips_on_route_id"
+  end
+
+  add_foreign_key "deliveries", "trips"
+  add_foreign_key "pickups", "trips"
+  add_foreign_key "trips", "routes"
+end
