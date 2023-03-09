8a1b9dc43dfbc7bd0663244f2e57b0f2d3c2e4b4
German Gallardo <ggallardoh.90@gmail.com>
2023-03-09 13:29:08 -0300

    Added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+route1 = Route.create(name: "Ruta 1")
+route2 = Route.create(name: "Ruta 2")
+
+trip1 = Trip.create(order: 1, route: route1)
+trip2 = Trip.create(order: 2, route: route1)
+trip3 = Trip.create(order: 3, route: route2)
+trip4 = Trip.create(order: 4, route: route2)
+
+Delivery.create(recipient: "German Gallardo",
+                address: "Las direcciones 123",
+                trip: trip1)
+Delivery.create(recipient: "German Gallarda",
+                address: "Las direcciones 1234",
+                trip: trip1)
+Delivery.create(recipient: "German Gallarde",
+                address: "Las direcciones 12345",
+                trip: trip1)
+Delivery.create(recipient: "German Gallardi",
+                address: "Las direcciones 123456",
+                trip: trip2)
+
+Pickup.create(sender: "German Gallardos",
+              address: "Las direcciones 123",
+              trip: trip1)
+Pickup.create(sender: "German Gallardas",
+              address: "Las direcciones 1234",
+              trip: trip1)
+Pickup.create(sender: "German Gallardes",
+              address: "Las direcciones 12345",
+              trip: trip2)
