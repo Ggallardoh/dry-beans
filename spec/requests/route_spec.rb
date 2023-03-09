92701b7299429236ffc89847d3ea6cd71588fac6
German Gallardo <ggallardoh.90@gmail.com>
2023-03-09 20:59:23 -0300

    Added tests for route controller

---
+++ spec/requests/route_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "Routes", type: :request do
+  let!(:route) { create(:route) }
+
+  describe "GET /api/v1/routes/:id" do
+    context 'when requesting an existing route' do
+      before do
+        get "/api/v1/routes/#{route.id}"
+      end
+      it 'returns status code 200' do
+        expect(response).to have_http_status :ok
+      end
+      it 'must return a Hash' do
+        expect(response.content_type).to include("application/json")
+      end
+    end
+    context 'when requesting a non-existing route' do
+      before do
+        get "/api/v1/routes/9999"
+      end
+      it 'returns status code 404' do
+        expect(response).to have_http_status :not_found
+      end
+    end
+  end
+end
